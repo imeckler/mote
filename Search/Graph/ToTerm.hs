b66887cac9f0b82301db4493e1d7c83933dfa382
Izaak Meckler <ihmeckler@gmail.com>
2015-05-19 12:35:18 -0500

    old graph type

--- Search/Graph/ToTerm.hs
+++ Search/Graph/ToTerm.hs
@@ -1,3 +1,20 @@
 module Search.Graph.ToTerm where
 
+import Search.Graph.Types
+import Search.Graph.Types.Vertex
+
+import qualified Data.Map as M
+import Data.Map (Map)
+
+data FreeNaturalGraph f = NaturalGraph
+  { incomingLabels :: [f]
+  , incomingSuccs  :: Map DummyVertex Vert
+  , incomingCount  :: Int
+
+  , outgoingPreds  :: Map DummyVertex Vert
+  , outgoingCount  :: Int
+
+  , digraph        :: Map Vertex (VertexData f)
+  }
+  deriving (Show)
 
